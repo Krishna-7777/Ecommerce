8eb6cbbe95395f745685e52b88986c1bebe5a2e3
Krishna-7777 <kchhabraji13@gmail.com>
2023-07-25 20:07:43 +0530

    auth login route with jwt

--- backend/routes/routes.auth.js
+++ backend/routes/routes.auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const UserModel = require("../models/user.model");
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
 const authRouter = express.Router()
 
@@ -23,6 +25,30 @@ authRouter.post('/register', async (ask, give) => {
         }
     } catch (error) {
         give.send({ msg: "Error occured while registering the user !", error: "Internal Server Error" })
+    }
+})
+
+authRouter.post('/login', async (ask, give) => {
+    let { email, password } = ask.body
+    try {
+        if (!email || !password ) {
+            give.send({ msg: "email, password is required in Request Body." })
+        } else {
+            let checkUser = await UserModel.findOne({ email }, { email: 1, password: 1 })
+            if (checkUser) {
+                let loggedIn=await bcrypt.compare(password,checkUser.password)
+                if(loggedIn){
+                    let token = jwt.sign({user:checkUser._id},process.env.secret)
+                    give.send({msg:"Login Success",token})
+                }else{
+                    give.send({msg:"Wrong Password"})
+                }
+            } else {
+                give.send({ msg: "Please Register First!" })
+            }
+        }
+    } catch (error) {
+        give.send({ msg: "Error occured while logging in the user !", error: "Internal Server Error" })
     }
 })
 

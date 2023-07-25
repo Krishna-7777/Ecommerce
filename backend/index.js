5cf3e325ef96df4c8f1fda6dfd89f74833d83a5e
Krishna-7777 <kchhabraji13@gmail.com>
2023-07-25 19:07:28 +0530

    basic express server

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const { connect } = require("./config/db");
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (ask, give) => {
+    give.send('Welcome to Ecommerce Backend')
+})
+
+app.listen(4000, () => {
+    try {
+        connect
+        console.log(`Connected to the DB and server is running at ${4000}`)
+    } catch (error) {
+        console.log(error);
+        console.log("Error in connecting to the DB")
+    }
+})

4f64b5c6490642762de7d622ff1e207ff86d39ec
Krishna-7777 <kchhabraji13@gmail.com>
2023-07-26 12:57:43 +0530

    add product to cart

--- backend/routes/routes.cart.js
+++ backend/routes/routes.cart.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const CartModel = require("../models/cart.model");
+const ProductModel = require("../models/product.model");
 
 const cartRouter = express.Router()
 
-cartRouter.get("/",async(ask, give)=>{
+cartRouter.get("/", async (ask, give) => {
     try {
-        let data = await CartModel.find({user:ask.user},{product:1,quantity:1})
+        let data = await CartModel.find({ user: ask.user }, { product: 1, quantity: 1 })
         give.send(data)
     } catch (error) {
         give.send({ msg: "Error occured while lisitng your cart !", error: "Internal Server Error" })
+    }
+})
+
+cartRouter.post("/:productId", async (ask, give) => {
+
+    let product = ask.params.productId
+    try {
+        let checkProduct = await ProductModel.findById(product, { _id: 1 })
+        try {
+            let cartItem = new CartModel({
+                user: ask.user,
+                quantity: 1,
+                product
+            })
+            await cartItem.save()
+            give.send({ msg: "Product added to your cart." })
+        } catch (error) {
+            give.send({ msg: "Error occured while adding a product to your cart !", error: "Internal Server Error" })
+        }
+    } catch (error) {
+        give.send({ msg: "The given Product ID is invalid" })
     }
 })
 
